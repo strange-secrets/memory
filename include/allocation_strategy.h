fda516920029d9f1e128719a9e8417e0bb1f0b42
nfactorial <nfactorial@btopenworld.com>
2020-01-27 12:29:01 +0000

    Extended documentation for allocation strategy enumeration

--- include/allocation_strategy.h
+++ include/allocation_strategy.h
@@ -5,9 +5,18 @@
 ////////////////////////////////////////////////////////////////////////////
 
 namespace ngen::memory {
+    //! \brief  Enumeration defining the various free-block selection algorithms that are supported.
+    //!
+    //! When an allocation request is made, the heap searches its list of available memory blocks to find a suitable
+    //! place where the requested memory can be allocated from. This enumeration specifies which type of selection
+    //! heuristic is applied when searching.
     enum class kAllocationStrategy {
         Invalid,
+
+        //! \brief  Chooses the first free block encountered that has the requested number of bytes available.
         First,
+
+        //! \brief  Scans all free blocks and selects the smallest free block that has the number of requested bytes available.
         Smallest
     };
 }
